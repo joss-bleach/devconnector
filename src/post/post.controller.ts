65e9908812f5d974f002871171ec960d8136ab18
Joss Bleach <jossbleach@josss-mbp.home>
2022-12-05 11:27:02 +0000

    Logic to create, delete and get posts.

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { JwtDecoded } from 'src/auth/strategy/jwt.strategy';
 import { GetCurrentUser } from 'src/user/decorators/current-user.decorator';
@@ -9,6 +17,7 @@ import { PostService } from './post.service';
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  // Create a new post
   @UseGuards(JwtGuard)
   @Post()
   async createPost(
@@ -16,5 +25,26 @@ export class PostController {
     @GetCurrentUser() user: JwtDecoded,
   ) {
     return await this.postService.createPost(user.id, body);
+  }
+
+  // Delete a post
+  @UseGuards(JwtGuard)
+  @Delete('/:postId')
+  async deletePost(@Param('postId') postId: string) {
+    return await this.postService.deletePost(postId);
+  }
+
+  // Get a post by ID
+  @UseGuards(JwtGuard)
+  @Get('/:postId')
+  async getPostById(@Param('postId') postId: string) {
+    return await this.postService.findPostById(postId);
+  }
+
+  // Get all posts by user
+  @UseGuards(JwtGuard)
+  @Get('/user/:userId')
+  async getAllUserPosts(@Param('userId') userId: string) {
+    return await this.postService.getAllUserPosts(userId);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { async } from 'rxjs';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { Post, PostDocument } from './schemas/post.schema';
 
@@ -12,14 +13,32 @@ export class PostService {
   ) {}
 
   findPostById = async (id: string) => {
-    return await this.postModel
+    const post = await this.postModel
       .findById(id)
       .populate('user', ['name', 'email', 'profileImage']);
+    if (!post) {
+      throw new NotFoundException('No post found with this ID.');
+    }
+    return post;
+  };
+
+  getAllUserPosts = async (userId: string) => {
+    const posts = await this.postModel
+      .find({ user: userId })
+      .populate('user', ['name', 'email', 'profileImage']);
+    if (posts.length === 0) {
+      throw new NotFoundException("This user hasn't posted.");
+    }
+    return posts;
   };
 
   createPost = async (userId: string, createPostDto: CreatePostDto) => {
     const postData = { ...createPostDto, user: userId };
     const newPost = await this.postModel.create(postData);
     return await newPost.save();
+  };
+
+  deletePost = async (postId: string) => {
+    return await this.postModel.findByIdAndDelete(postId);
   };
 }
